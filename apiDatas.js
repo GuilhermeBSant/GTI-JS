b6e47f1f06dec88de506b9cb2095483bbee3f9a3
Guilherme Santiago <guilhermebscontact@gmail.com>
2022-08-25 03:36:13 -0300

    projeto finalizado

--- apiDatas.js
+++ apiDatas.js
@@ -1,7 +1,6 @@
 export async function getDatas(){
     let response = await fetch('https://quiz-trainee.herokuapp.com/questions')
     let data = await response.json()
-    console.log(data)
     return data
 }
 
--- index.js
+++ index.js
@@ -1,43 +1,143 @@
-import {getDatas} from './apiDatas.js';
+import { getDatas } from "./apiDatas.js";
 
-let teste = await getDatas()
+let teste = await getDatas();
+let question = 0;
+let totalOfValues = 0;
 //opções: title, id, options(array com as respostas[value, id, question_id,answer])
 
+(async function startForm(){
+    document.querySelector("#title").innerText = "GTI QUIZ"
 
-function createOptions(index){
-    let form = document.querySelector("#form");
-    document.getElementById('title').innerText = teste[0].title
+    let texto = document.createElement("p")
+    texto.innerText = `Bem-vindo(a) ao quiz da GTI`
 
-    teste[index].options.forEach( (e, i) =>{
-        console.log(e)
+    let button = document.createElement("button")
+    button.innerText = "Clique aqui para iniciar"
+    button.classList.add("start-end-button")
+    button.addEventListener("click", function(){
+        texto.remove()
+        button.remove()
+        createFormElements(question);
+    })
 
-    let div = document.createElement('div')
-    div.classList.add('item')
+    document.querySelector(".container").appendChild(texto)
+    document.querySelector(".container").appendChild(button)
 
-    let optionCheckbox = document.createElement("input")
-    optionCheckbox.type = "radio"
-    optionCheckbox.name = "resposta"
-    optionCheckbox.value = teste[index].options[i].value
+})()
 
-    let optionLabel = document.createElement("label")
+function checkHighestValue(highArray) {
+    let highestValue = 0;
+    highArray.forEach((e) => {
+      let localValue = 0;
+      e.options.forEach((options, i) => {
+        if (options.value > localValue) {
+          localValue = options.value;
+        }
+      });
+      highestValue += localValue;
+    });
+    return highestValue;
+  }
 
-    let optionAnswer = document.createElement("p")
-    optionAnswer.innerText = teste[0].options[i].answer
+async function createFormElements(index) {
+  let form = document.querySelector("#form");
+  document.getElementById("title").innerText = teste[0].title;
 
-    optionLabel.appendChild(optionCheckbox)
-    optionLabel.appendChild(optionAnswer)
+  teste[index].options.forEach((e, i) => {
 
-    form.appendChild(div)
-    div.appendChild(optionLabel)
+    let div = document.createElement("div");
+    div.classList.add("item");
+
+    let optionCheckbox = document.createElement("input");
+    optionCheckbox.type = "radio";
+    optionCheckbox.name = "resposta";
+    optionCheckbox.value = teste[index].options[i].value;
+
+    let optionLabel = document.createElement("label");
+
+    let optionAnswer = document.createElement("p");
+    optionAnswer.classList.add("p")
+    optionAnswer.innerText = teste[index].options[i].answer;
+
+    optionLabel.appendChild(optionCheckbox);
+    optionLabel.appendChild(optionAnswer);
+
+    form.appendChild(div);
+    div.appendChild(optionLabel);
+  });
+
+  let button = document.createElement("button");
+  button.innerText = "Enviar";
+  button.type = "submit"
+  button.disabled =true
+  button.id = "send-data";
+  form.appendChild(button);
+
+document.getElementsByName("resposta").forEach(e=>{
+    e.addEventListener("change", function(){
+        button.disabled=false
+    })
+})
+
+  let formButton = document.getElementById("send-data")
+    formButton.addEventListener('click', e =>{
+    
+    e.preventDefault()
+
+    let selectedValue = document.querySelector('[name=resposta]:checked').value;
+    question++;
+    getValueAndSkipQuestion(question, selectedValue)
+})
+}
+
+function updateFormElements(question){
+    document.getElementById("title").innerText = teste[question].title;
+    document.querySelectorAll(".p").forEach((e,i)=>{
+        e.innerText = teste[question].options[i].answer;
+    })
+    document.querySelectorAll('[name=resposta]').forEach((e,i)=>{
+        e.value = teste[question].options[i].value
     })
 }
 
-function createButton(){
-    let form = document.querySelector("#form");
-    let button = document.createElement("button")
-    button.innerText = "Enviar"
-    form.appendChild(button)
+async function getValueAndSkipQuestion(question, selectedValue){
+    
+    totalOfValues += parseInt(selectedValue);
+
+    if(question < teste.length){
+        updateFormElements(question)
+    }else{
+        finishForm(totalOfValues)
+    }
 }
 
-createOptions(0)
-createButton()
+function finishForm(totalOfValuesForm){
+    document.querySelector("#form").remove()
+    document.querySelector("#title").innerText = "Resultados"
+    
+    let maxValue = checkHighestValue(teste)
+    let percentBasedOnValue = (totalOfValuesForm/maxValue*100).toFixed(2)
+
+    let pontuacao = document.createElement("p")
+    pontuacao.innerText = `Sua pontuação foi de: ${percentBasedOnValue}%`
+
+    let button = document.createElement("button")
+    button.innerText = "Reiniciar o quiz"
+    button.classList.add("start-end-button")
+    button.addEventListener("click", function(){
+        question =0;
+        totalOfValues=0;
+        button.remove()
+        pontuacao.remove()
+
+        let form = document.createElement("form")
+        form.id="form"
+        document.querySelector(".container").appendChild(form)
+
+        createFormElements(question)
+    })
+
+    document.querySelector(".container").appendChild(pontuacao)
+    document.querySelector(".container").appendChild(button)
+}
+
